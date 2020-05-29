af6d6a11a6e5b3c9a0c4ada6904d46dd2f14f130
Jasmin Oster <j.oster@outlook.de>
2020-05-29 11:58:45 +0200

    Zip the extension on build

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const ZipPlugin = require('zip-webpack-plugin');
+
+const rootPath = __dirname + "/../";
+const manifest = require(rootPath + "/public/manifest.json");
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new ZipPlugin({
+            include: ["dist/"],
+            path: rootPath + "/zip/",
+            filename: `extension-v${manifest.version}.zip`,
+        })
+    ]
 });

62ded4cf79356986af4355accfdc659545ae81a1
Jasmin Oster <j.oster@outlook.de>
2020-05-30 12:12:05 +0200

    Update the regex to start at line beginning

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const ALREADY_PARSED_ATTRIBUTE_NAME = "data-cn-parsed";
-export const CONTENT_NOTE_REGEX = /[TtCc][WwNn]:?\s+?([\S ]+)/;
+export const CONTENT_NOTE_REGEX = /^[TtCc][WwNn]:?\s+?([\S ]+)/;

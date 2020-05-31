51e61839bfb33578bd42c813e63c7a9c7a345ffe
Jasmin Oster <j.oster@outlook.de>
2020-05-31 16:19:32 +0200

    Do not omit version number in ZIP
    Makes it easier for the CI artifacts.

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,14 +3,13 @@ const common = require('./webpack.common.js');
 const ZipPlugin = require('zip-webpack-plugin');
 
 const rootPath = __dirname + "/../";
-const manifest = require(rootPath + "/public/manifest.json");
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new ZipPlugin({
             path: rootPath + "/zip/",
-            filename: `extension-v${manifest.version}.zip`,
+            filename: `extension.zip`,
         })
     ]
 });

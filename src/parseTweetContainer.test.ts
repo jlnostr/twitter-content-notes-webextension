57e501024d317ebfbc5f39ffd9dc49df156b29b9
Jasmin Oster <j.oster@outlook.de>
2020-06-01 23:28:21 +0200

    Add tests

--- src/parseTweetContainer.test.ts
+++ src/parseTweetContainer.test.ts
@@ -1,8 +1,28 @@
-import { combineContentNotes } from "./parseTweetContainer";
+import { combineContentNotes, getKeywords } from "./parseTweetContainer";
 
 test("content notes are combined", () => {
 	const input = ["first", "second"];
 	const result = combineContentNotes(input);
 
-	expect(result).toBe("first; second");
+	expect(result).toBe("first, second");
+});
+
+describe.each([
+	[["test", "test"], "test"],
+	[["racism, police", "police"], "racism, police"],
+	[["my test", "another test"], "my test, another test"],
+	[["my test", "my test"], "my test"],
+])("duplicates", (input, expectedOutput) => {
+	test("are removed", () => {
+		expect(combineContentNotes(input)).toBe(expectedOutput);
+	});
+});
+
+describe.each([
+	["test", ["test"]],
+	["racism, police", ["racism", "police"]],
+	["racism,      police", ["racism", "police"]],
+])("export contains valid", (input, expectedOutput) => {
+	const result = getKeywords(input);
+	expect(result).toStrictEqual(expectedOutput);
 });
